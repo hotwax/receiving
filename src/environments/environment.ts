e1bfdbfa0e9d5aa58dd8e0a5d27389b0b14e0365
Deepak Dixit <deepak.dixit@hotwax.co>
2021-10-20 14:05:51 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   BASE_URL: 'https://dev-hc.hotwax.io/api/',
   DEFAULT_IMAGE: 'assets/imgs/defaultImage.png',
   RESOURCE_URL: 'https://dev-resources.hotwax.io/',
-  SHOPIFY_API_KEY: 'a11d903abadcbec02d1fc134678fb30f',
-  SHOPIFY_API_SECRET: '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',
+  SHOPIFY_API_KEY: '',
+  SHOPIFY_API_SECRET: '',
   SHOPIFY_REDIRECT_URI: 'https://receiving-dev.web.app/'
 };
 
