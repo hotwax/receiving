b9dcaca5c2a1a08982122952ce63ec5552fa58c0
Azkya Khan <azkya.khan@hotwax.co>
2021-11-15 19:09:07 +0530

    Improve code of settings page functionality (#1up74vv)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,16 +8,14 @@
     
     <ion-content :fullscreen="true">
       <ion-list>
-        <!-- Select store -->
-        <ion-item>
+       <ion-item>
           <ion-icon :icon="storefrontOutline" slot="start" />
-          <ion-label>{{$t("store")}}</ion-label>
-          <ion-select interface="popover" :placeholder="$t('store name')" :selected-text="currentFacility.facilityId" @ionChange="setFacility($event)">
-            <ion-select-option v-for="facility in userProfile.facilities" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.facilityId }}</ion-select-option>
+          <ion-label>{{$t("Store")}}</ion-label>
+          <ion-select interface="popover" :placeholder="$t('store name')" :value="currentFacility.facilityId" @ionChange="setFacility($event)">
+            <ion-select-option v-for="facility in (userProfile ? userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
           </ion-select>
         </ion-item>
 
-        <!-- Profile of user logged in -->
         <ion-item>
           <ion-icon :icon="personCircleOutline" slot="start" />
           <ion-label>{{ userProfile !== null ? userProfile.partyName : '' }}</ion-label>
@@ -29,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { alertController, IonButton, IonContent, IonHeader,IonIcon, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar, popoverController } from '@ionic/vue';
+import { alertController, IonButton, IonContent, IonHeader,IonIcon, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { ellipsisVertical, personCircleOutline, storefrontOutline} from 'ionicons/icons'
 import { mapGetters, useStore } from 'vuex';
