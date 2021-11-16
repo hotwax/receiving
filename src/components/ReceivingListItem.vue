f9957609c3e3ff825518236fbb861924a63621ff
Azkya Khan <azkya.khan@hotwax.co>
2021-11-16 17:14:20 +0530

    Created receiving list item component (#1uvq022)

---
+++ src/components/ReceivingListItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <ion-list>
+    <ion-item @click="() => router.push('/shipment')">
+      <ion-label>
+        <h2>SHPMNT_001</h2>
+        <p>3 {{ $t("items") }}</p>
+      </ion-label>
+      <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
+    </ion-item>
+    <ion-item>
+      <ion-label>
+        <h2>SHPMNT_002</h2>
+        <p>2 {{ $t("items") }}</p>
+      </ion-label>
+      <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
+    </ion-item>
+    <ion-item>
+      <ion-label>
+        <h2>SHPMNT_003</h2>
+        <p>6 {{ $t("items") }}</p>
+      </ion-label>
+      <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
+    </ion-item>
+    <ion-item>
+      <ion-label>
+        <h2>SHPMNT_004</h2>
+        <p>4 {{ $t("items") }}</p>
+      </ion-label>
+      <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
+    </ion-item>
+    <ion-item>
+      <ion-label>
+        <h2>SHPMNT_005</h2>
+        <p>1 {{ $t("item") }}</p>
+      </ion-label>
+      <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
+    </ion-item>
+  </ion-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote
+} from '@ionic/vue'
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: "ReceivingListItem",
+  components: {
+    IonItem,
+    IonLabel,
+    IonList,
+    IonNote
+  },
+  setup(){
+    const router = useRouter();
+    return {
+      router
+    }
+  }
+})
+</script>
--- src/views/Receiving.vue
+++ src/views/Receiving.vue
@@ -9,43 +9,9 @@
     <ion-content :fullscreen="true">
       <div>
         <ion-searchbar :placeholder="$t('Scan ASN to start receiving')"></ion-searchbar>
-        <ion-list>
-          <ion-item @click="() => router.push('/shipment')">
-            <ion-label>
-              <h2>SHPMNT_001</h2>
-              <p>3 {{ $t("items") }}</p>
-            </ion-label>
-            <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
-          </ion-item>
-          <ion-item>
-            <ion-label>
-              <h2>SHPMNT_002</h2>
-              <p>2 {{ $t("items") }}</p>
-            </ion-label>
-            <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
-          </ion-item>
-          <ion-item>
-            <ion-label>
-              <h2>SHPMNT_003</h2>
-              <p>6 {{ $t("items") }}</p>
-            </ion-label>
-            <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
-          </ion-item>
-          <ion-item>
-            <ion-label>
-              <h2>SHPMNT_004</h2>
-              <p>4 {{ $t("items") }}</p>
-            </ion-label>
-            <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
-          </ion-item>
-          <ion-item>
-            <ion-label>
-              <h2>SHPMNT_005</h2>
-              <p>1 {{ $t("item") }}</p>
-            </ion-label>
-            <ion-note slot="end">{{ $t("Shipped") }}</ion-note>
-          </ion-item>
-        </ion-list>
+        
+        <ReceivingListItem />
+
         <div class="ion-text-center">
           <ion-button fill="outline" color="dark"><ion-icon :icon="cloudDownloadOutline" slot="start" />{{ $t("Load more shipments") }}</ion-button>
         </div>
@@ -55,10 +21,10 @@
 </template>
 
 <script lang="ts">
-import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonNote, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { cloudDownloadOutline } from 'ionicons/icons'
-import { useRouter } from 'vue-router';
+import ReceivingListItem from '@/components/ReceivingListItem.vue'
 
 export default defineComponent({
   name: 'Receiving',
@@ -67,20 +33,15 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonIcon,
-    IonItem,
-    IonLabel,
-    IonList,
-    IonNote,
     IonSearchbar,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    ReceivingListItem
   },
   setup(){
-    const router = useRouter();
     return {
-      cloudDownloadOutline,
-      router
+      cloudDownloadOutline
     }
   }
 });
