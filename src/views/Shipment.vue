0bbbac1d682b900934d21b9a2164df3f18a6d378
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-12-31 22:33:12 +0530

    Adding barcode functionality (#1xhmv5x)

--- src/views/Shipment.vue
+++ src/views/Shipment.vue
@@ -16,9 +16,15 @@
           <h1>{{ $t("Shipment ID") }} : {{ items.shipmentId }}</h1>
         </ion-item>
         <ion-item>
+          <ion-label>{{ $t("Scan Items") }}</ion-label>
           <ion-label>
             <ion-input :placeholder="$t('Scan barcodes to receive')"></ion-input>
           </ion-label>
+          <ion-buttons>
+            <ion-button class="action-button" fill="outline" color="secondary" @click="scanCode()">
+            <ion-icon slot="start" :icon="barcodeOutline" />{{ $t("Scan") }}
+            </ion-button>
+          </ion-buttons>
         </ion-item>
         <ion-card v-for="item in items.items" :key="item.id">
           <div class="product-info">
@@ -76,13 +82,14 @@ import {
   alertController,
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
-import { add, checkmarkDone } from 'ionicons/icons';
+import { add, checkmarkDone, barcodeOutline } from 'ionicons/icons';
 import { mapGetters, useStore } from "vuex";
 import AddProductModal from '@/views/AddProductModal.vue'
 import Image from "@/components/Image.vue";
 import { useRouter } from 'vue-router';
 import { showToast } from '@/utils'
 import { translate } from '@/i18n'
+import Scanner from "./Scanner.vue";
 
 export default defineComponent({
   name: "Shipment details",
@@ -172,6 +179,19 @@ export default defineComponent({
         }
       })
     },
+    async scanCode () {
+      const modal = await modalController
+        .create({
+          component: Scanner,
+        });
+        modal.onDidDismiss()
+      .then((result) => {
+        console.log(result.role)
+        //result : value of the scanned barcode/QRcode
+        console.log(result);
+    });
+      return modal.present();
+    },
   }, 
   setup() {
     const store = useStore(); 
@@ -179,6 +199,7 @@ export default defineComponent({
 
     return {
       add,
+      barcodeOutline,
       checkmarkDone,
       store,
       router
