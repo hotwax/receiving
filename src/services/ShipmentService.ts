434dcee61545e0b8436cc56c61d0e162f66351c1
Yashi002 <vermayashi002@gmail.com>
2022-01-10 14:27:45 +0530

    add changes

---
+++ src/services/ShipmentService.ts
@@ -0,0 +1,43 @@
+import api from '@/api';
+
+const fetchShipments = async (query: any): Promise <any>  => {
+  return api({
+    url: "incoming-shipments", 
+    method: "post",
+    data: query,
+    cache: true
+  });
+}
+
+const getShipmentProduct= async (query: any): Promise<any> => {
+  return api({
+    url: "shipment-detail",
+    data: query,
+    method: 'post'
+  });
+}
+
+const receiveShipmentItems = async (query: any): Promise <any> => {
+  return api({
+    url: "receiveShipmentItem",
+    method: "post",
+    data: query
+  });
+}
+
+const updateShipments = async (query: any): Promise <any> => {
+  return api({
+    url: "updateShipment",
+    method: "post",
+    data: query
+  })
+}
+
+
+export const ShipmentService = {
+  fetchShipments,
+  getShipmentProduct,
+  receiveShipmentItems,
+  updateShipments
+}
+
