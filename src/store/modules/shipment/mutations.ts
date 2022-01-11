a8879e71fec82afb5ee1be31b71f2ab49a96a4b3
Yashi Verma <50695892+Yashi002@users.noreply.github.com>
2022-01-11 17:13:54 +0530

    Update src/store/modules/shipment/mutations.ts
    Co-authored-by: Aditya Sharma <iamadityasharma7@gmail.com>

--- src/store/modules/shipment/mutations.ts
+++ src/store/modules/shipment/mutations.ts
@@ -4,7 +4,7 @@ import * as types from './mutation-types'
 
 const mutations: MutationTree <ShipmentState> = {
   [types.SHIPMENT_LIST_UPDATED] (state, payload) {
-      state.shipments.list=payload.shipments;
+      state.shipments.list = payload.shipments;
   },
   [types.SHIPMENT_CURRENT_UPDATED] (state, payload) {
     state.current = payload.current;
