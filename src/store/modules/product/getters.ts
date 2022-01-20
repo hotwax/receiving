41d23a80439f4e69a93e649aa122ecf2d7cb1f2f
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2022-01-20 13:39:49 +0530

    Removed unwanted code (#dzkchn)

--- src/store/modules/product/getters.ts
+++ src/store/modules/product/getters.ts
@@ -4,7 +4,7 @@ import RootState from '../../RootState'
 
 const getters: GetterTree <ProductState, RootState> = {
   getProduct: (state) => (productId: string) => {
-    return state.cached[productId] ? state.cached[productId] : 0
+    return state.cached[productId] ? state.cached[productId] : {};
   }
 }
 export default getters
