cdbddd2ac09e6d088030065a705c06a553db5f7b
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2022-02-15 13:36:02 +0530

    Changed name of order mutation(#1wmz45q).

--- src/store/modules/order/mutations.ts
+++ src/store/modules/order/mutations.ts
@@ -3,7 +3,7 @@ import OrderState from './OrderState'
 import * as types from './mutation-types'
 
 const mutations: MutationTree <OrderState> = {
-  [types.ORDER_PURCHASE_UPDATED] (state, payload) {
+  [types.ORDER_PRCHS_ORDRS_UPDATED] (state, payload) {
     state.purchaseOrders.list = payload.list;
     state.purchaseOrders.total = payload.total;
   }
