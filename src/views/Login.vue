578b9f6602c1937b388c8479f08b3e20c031c5e0
Aditya Sharma <iamadityasharma7@gmail.com>
2022-10-13 14:43:16 +0530

    Improved: processed & stored instanceURL once (#2wv2fh0)
    In order to avoid redundant efforts processed and stored instanceURL to a variable and used throughout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -71,7 +71,8 @@ export default defineComponent({
   },
   methods: {
     login: function () {
-      if(!this.baseURL) this.store.dispatch("user/setUserInstanceUrl", this.alias[this.instanceUrl.trim().toLowerCase()] ? this.alias[this.instanceUrl.trim().toLowerCase()] : this.instanceUrl.trim().toLowerCase());
+      const instanceURL = this.instanceUrl.trim().toLowerCase();
+      if(!this.baseURL) this.store.dispatch("user/setUserInstanceUrl", this.alias[instanceURL] ? this.alias[instanceURL] : instanceURL);
       const { username, password } = this;
       this.store.dispatch("user/login", { username: username.trim(), password }).then((data: any) => {
         if (data.token) {
