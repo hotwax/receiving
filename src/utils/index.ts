acdde41defd1896c4b63996fc22bd228e87bf301
shashwatbangar <shashwatbangar@gmail.com>
2023-01-17 11:52:58 +0530

    Fixed: using now utc dateTime format(#2dewerv)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,7 +35,7 @@ const handleDateTimeInput = (dateTimeValue: any) => {
   // TODO Handle it in a better way
   // Remove timezone and then convert to timestamp
   // Current date time picker picks browser timezone and there is no supprt to change it
-  const dateTime = DateTime.fromISO(dateTimeValue, { setZone: true}).toFormat("yyyy-MM-dd'T'HH:mm:ss")
+  const dateTime = DateTime.fromISO(dateTimeValue, { zone: 'utc' }).toFormat("yyyy-MM-dd'T'HH:mm:ss")
   return DateTime.fromISO(dateTime).toMillis()
 }
 
