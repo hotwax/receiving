3c70c010ce6e58e4451a728c93f00b5001feffd1
Disha Talreja <dishatalreja1202@gmail.com>
2023-02-21 18:07:14 +0530

    improved: moved logic in methods from template(#85zrnfx68)

--- src/views/PurchaseOrderDetail.vue
+++ src/views/PurchaseOrderDetail.vue
@@ -73,7 +73,7 @@
 
             <div class="qty-progress">
               <!-- TODO: improve the handling of quantityAccepted -->
-              <ion-progress-bar :color="parseInt(item.quantityAccepted)/(item.quantity - getPOItemAccepted(item.productId)) > 1 ? 'danger' : 'primary'" :value="(parseInt(item.quantityAccepted) +  getPOItemAccepted(item.productId))/(item.quantity)" />
+              <ion-progress-bar :color="areItemsReceivedGreaterThanItemsOrdered(item) ? 'danger' : 'primary'" :value="getNumberOfItemsReceived(item)" />
             </div>
 
             <div class="po-item-history">
@@ -176,6 +176,12 @@ export default defineComponent({
     })
   },
   methods: {
+    getNumberOfItemsReceived(item: any){
+      return (parseInt(item.quantityAccepted) +  this.getPOItemAccepted(item.productId))/(item.quantity)
+    },
+    areItemsReceivedGreaterThanItemsOrdered(item: any){
+      return item.quantityAccepted > (item.quantity - this.getPOItemAccepted(item.productId))
+    },
     async openImage(imageUrl: string, productName: string) {
       const imageModal = await modalController.create({
         component: ImageModal,
