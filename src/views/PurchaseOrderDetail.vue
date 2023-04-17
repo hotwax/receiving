5f2c52d058884dd57c0f408a38a13ce6a97ab809
k2maan <ketuman333@gmail.com>
2023-04-17 11:58:03 +0530

    Improved: changed UI toast message for copying internal ID on PO details page (#85zrx4mju)

--- src/views/PurchaseOrderDetail.vue
+++ src/views/PurchaseOrderDetail.vue
@@ -23,7 +23,7 @@
           </ion-item>
 
           <div class="doc-meta">
-            <ion-chip @click="copyToClipboard(order.orderId)">{{ order.orderId }}<ion-icon :icon="copyOutline"/></ion-chip>
+            <ion-chip @click="copyOrderId(order.orderId)">{{ order.orderId }}<ion-icon :icon="copyOutline"/></ion-chip>
             <ion-badge :color="order.orderStatusId === 'ORDER_CREATED' ? 'medium' : 'primary'">{{ order.orderStatusDesc }}</ion-badge>
           </div>
         </div>
@@ -117,7 +117,7 @@ import {
   IonItem,
   IonInput,
   IonLabel,
-  IonPage,
+  IonPage,         
   IonProgressBar,
   IonThumbnail,
   IonTitle,
@@ -135,8 +135,9 @@ import Scanner from "@/components/Scanner.vue"
 import AddProductToPOModal from '@/views/AddProductToPOModal.vue'
 import LocationPopover from '@/components/LocationPopover.vue'
 import ImageModal from '@/components/ImageModal.vue';
-import { copyToClipboard, hasError, productHelpers } from '@/utils';
+import { hasError, productHelpers, showToast } from '@/utils';
 import { Actions, hasPermission } from '@/authorization'
+import { Plugins } from '@capacitor/core';
 
 export default defineComponent({
   name: "PurchaseOrderDetails",
@@ -255,6 +256,15 @@ export default defineComponent({
           return true;
         }
       })
+    },
+    async copyOrderId(orderId: string) {
+      const { Clipboard } = Plugins;
+
+      await Clipboard.write({
+        string: orderId
+      }).then(() => {
+        showToast(this.$t("Internal ID saved to clipboard"));
+      })
     }
   }, 
   ionViewWillEnter() {
@@ -272,7 +282,6 @@ export default defineComponent({
       cameraOutline,
       checkmarkDone,
       copyOutline,
-      copyToClipboard,
       hasPermission,
       productHelpers,
       router,
