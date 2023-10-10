f5f48773483bbf6b39960469151617a3e950c1d0
Sanskar Soni <sonisanskar223@gmail.com>
2023-10-10 10:57:13 +0530

    Implemented: support for single logout

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -219,9 +219,14 @@ export default defineComponent({
         {
           text: this.$t('Ok'),
           handler: () => {
-            this.store.dispatch('product/clearUploadProducts');
-            this.store.dispatch('user/logout').then(() => {
-            this.router.push('/login');
+            this.store.dispatch('user/logout', { isUserUnauthorised: false }).then((redirectionUrl) => {
+              this.store.dispatch('product/clearUploadProducts');
+
+              // if not having redirection url then redirect the user to launchpad
+              if (!redirectionUrl) {
+                const redirectUrl = window.location.origin + '/login'
+                window.location.href = `${process.env.VUE_APP_LOGIN_URL}?isLoggedOut=true&redirectUrl=${redirectUrl}`
+              }
             })
           }
         }]
@@ -229,12 +234,16 @@ export default defineComponent({
       await alert.present();
     },
     logout () {
-      this.store.dispatch('user/logout').then(() => {
+    this.store.dispatch('user/logout', { isUserUnauthorised: false }).then((redirectionUrl) => {
         this.store.dispatch('shipment/clearShipments');
         this.store.dispatch('return/clearReturns');
         this.store.dispatch("party/resetReceiversDetails");
-        const redirectUrl = window.location.origin + '/login'
-        window.location.href = `${process.env.VUE_APP_LOGIN_URL}?isLoggedOut=true&redirectUrl=${redirectUrl}`
+
+        // if not having redirection url then redirect the user to launchpad
+        if (!redirectionUrl) {
+          const redirectUrl = window.location.origin + '/login'
+          window.location.href = `${process.env.VUE_APP_LOGIN_URL}?isLoggedOut=true&redirectUrl=${redirectUrl}`
+        }
       })
     },
     goToLaunchpad() {
