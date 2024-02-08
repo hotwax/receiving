86653a0f41b49f8a161267bd927d98a354b7d5aa
Ritika-Patel08 <ritikapatel8870@gmail.com>
2024-02-08 15:08:32 +0530

    Fixed: error handling in login process in case of object in error (dxp-258)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -97,7 +97,7 @@ const actions: ActionTree<UserState, RootState> = {
       // TODO Check if handling of specific status codes is required.
       showToast(translate('Something went wrong while login. Please contact administrator'));
       console.error("error", err);
-      return Promise.reject(new Error(err))
+      return Promise.reject(err instanceof Object ? err : new Error(err))
     }
   },
 
