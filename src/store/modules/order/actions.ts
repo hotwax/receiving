5aae9be25505be9e1037dc906dd1f47dc4b9480d
Dhiraj Nagar <119936391+Dhiraj1405@users.noreply.github.com>
2024-03-21 16:58:58 +0530

    Update actions.ts

--- src/store/modules/order/actions.ts
+++ src/store/modules/order/actions.ts
@@ -48,7 +48,7 @@ const actions: ActionTree<OrderState, RootState> = {
       commit(types.ORDER_CURRENT_UPDATED, state.current )
       showToast(translate("Scanned successfully.", { itemName: payload }))
     } else {
-      showToast(translate("Failed to scan:", { itemName: payload }))
+      showToast(translate("Scanned item is not present within the shipment:", { itemName: payload }))
     }
   },
   async addOrderItem ({ commit }, payload) {
