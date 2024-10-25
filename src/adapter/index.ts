9d9fb850a13ddba831a26b2b903937495ab974b7
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2024-10-25 12:37:41 +0530

    Improved: Passed method to fetch good identification types in order to display product identifier options dynamically (#345).

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -1,9 +1,10 @@
-import { api, client, getConfig, getUserFacilities, hasError, initialise, logout, resetConfig, updateInstanceUrl, updateToken, setUserTimeZone,
+import { api, client, getConfig, fetchGoodIdentificationTypes, getUserFacilities, hasError, initialise, logout, resetConfig, updateInstanceUrl, updateToken, setUserTimeZone,
   getAvailableTimeZones, getProductIdentificationPref, setProductIdentificationPref} from '@hotwax/oms-api'
 
 export {
   api,
   client,
+  fetchGoodIdentificationTypes,
   getConfig,
   getUserFacilities,
   getProductIdentificationPref,
