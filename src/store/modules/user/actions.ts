0d6d408a96f636b794fe983220edd67c2c12d71a
R-Sourabh <rsourabh002@gmail.com>
2025-01-28 11:04:30 +0530

    Fixed: Allowed users to successfully update product identifiers(bopis-494)

--- src/store/modules/user/actions.ts
+++ src/store/modules/user/actions.ts
@@ -71,8 +71,9 @@ const actions: ActionTree<UserState, RootState> = {
         return uniqueFacilities
       }, []);
 
-      const currentFacility: any = getCurrentFacilityId();
-      const currentEComStore = await UserService.getEComStores(token, currentFacility?.facilityId);
+      const currentFacilityId: any = getCurrentFacilityId();
+      const currentEComStore = await UserService.getEComStores(token, currentFacilityId);
+      await useUserStore().setEComStorePreference(currentEComStore);
       const productStoreId = currentEComStore?.productStoreId;
 
       await useProductIdentificationStore().getIdentificationPref(productStoreId)
@@ -90,7 +91,7 @@ const actions: ActionTree<UserState, RootState> = {
       commit(types.USER_PERMISSIONS_UPDATED, appPermissions);
       commit(types.USER_TOKEN_CHANGED, { newToken: token })
       // Get facility location of selected facility
-      dispatch('getFacilityLocations', currentFacility.facilityId);
+      dispatch('getFacilityLocations', currentFacilityId);
       // TODO: fetch product identifications from enumeration instead of storing it in env
       this.dispatch('util/getForceScanSetting', currentEComStore?.productStoreId);
       this.dispatch('util/getBarcodeIdentificationPref', currentEComStore?.productStoreId);
@@ -174,7 +175,7 @@ const actions: ActionTree<UserState, RootState> = {
   async setFacility ({ commit, dispatch }, facilityId) {
     const token = store.getters['user/getUserToken'];
     const eComStore = await UserService.getEComStores(token, facilityId);
-
+    await useUserStore().setEComStorePreference(eComStore);
     commit(types.USER_CURRENT_ECOM_STORE_UPDATED, eComStore);
     await dispatch('getFacilityLocations', facilityId)
     eComStore?.productStoreId ? this.dispatch('util/getForceScanSetting', eComStore.productStoreId) : this.dispatch('util/updateForceScanStatus', false)
