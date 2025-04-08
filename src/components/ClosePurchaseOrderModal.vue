ebe0f0aaf1c1ae0ab825b69a17fcaf94c97ebe58
Aashutosh Soni <aashutosh.soni@hotwaxsystems.com>
2025-04-08 19:49:59 +0530

    Implemented: serverErrorAlert method to display server errors and option to copy error or dismiss in ClosePurchaseOrderModal at Purchase Order tab. (#449)

--- src/components/ClosePurchaseOrderModal.vue
+++ src/components/ClosePurchaseOrderModal.vue
@@ -67,7 +67,7 @@ import { mapGetters, useStore } from 'vuex'
 import { OrderService } from "@/services/OrderService";
 import { DxpShopifyImg, translate, getProductIdentificationValue, useProductIdentificationStore } from '@hotwax/dxp-components';
 import { useRouter } from 'vue-router';
-import { hasError } from '@/utils';
+import { hasError, copyToClipboard } from '@/utils';
 
 export default defineComponent({
   name: "ClosePurchaseOrderModal",
@@ -116,7 +116,28 @@ export default defineComponent({
           handler: async() => {
             await this.updatePOItemStatus()
             modalController.dismiss()
-            this.router.push('/purchase-orders')
+          }
+        }]
+      });
+      return alert.present();
+    },
+    async serverErrorAlert(message: string) {
+      const alert = await alertController.create({
+        header: 'Error while receiving',
+        message,
+        buttons: [{
+          text: translate('Copy & Dismiss'),
+          role: 'copyAndDismiss',
+          handler: async() => {
+            modalController.dismiss()
+            copyToClipboard(message)
+          }
+        },
+        {
+          text: translate('Dismiss'),
+          role: 'dismiss',
+          handler: async() => {
+            modalController.dismiss()
           }
         }]
       });
@@ -178,6 +199,7 @@ export default defineComponent({
         }
       } catch(error: any) {
         hasFailedItems = true;
+        await this.serverErrorAlert(error.message || 'Failed to update the status of purchase order items.');
       }
 
       if(hasFailedItems){
@@ -211,6 +233,7 @@ export default defineComponent({
         }
         this.store.dispatch("order/updatePurchaseOrders", { purchaseOrders })
       }
+      this.router.push('/purchase-orders');
     },
     isEligibleToClosePOItems() {
       return this.order.items.some((item: any) => item.isChecked && this.isPOItemStatusPending(item))
