fa9d5391b23f2fff338e0496bb0bd2a36ea63390
Yash Maheshwari <yashmaheshwari551@gmail.com>
2025-12-17 11:05:26 +0530

    Added: the env example file(#548)

--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,7 @@ module.exports = {
       alias: {
         vue: path.resolve('./node_modules/vue')
       }
-    },
-  },
-  devServer: {
-    allowedHosts: "all",
-    host: '0.0.0.0', // Allows access from any IP address
-    port: 8100, // Or your specific port for Ionic/Vue
-    client: {
-      webSocketURL: 'auto://0.0.0.0:0/ws'
-    },
+    }
   },
   runtimeCompiler: true,
   transpileDependencies: ['@hotwax/dxp-components']
