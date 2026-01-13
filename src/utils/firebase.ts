b82448496ca320f8ca0e7100d8361796ac642962
R-Sourabh <rsourabh002@gmail.com>
2026-01-13 16:18:39 +0530

    Improved: removed unnecessary comment of generateTopicName firebase utility function(#643)

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -11,8 +11,6 @@ const generateDeviceId = () => {
   return deviceId ? deviceId : (DateTime.now().toFormat('ddMMyy') + String(DateTime.now().toMillis()).slice(-6))
 }
 
-// TODO: Need to check this topic name generation logic with backend team.
-// topic name: oms-facilityId-enumId(enumCode)
 const generateTopicName = (facilityId: string, enumId: string) => {
   const userProfile = store.getters['user/getUserProfile'];
   return `${userProfile.omsInstanceName}-${facilityId}-${enumId}`;
