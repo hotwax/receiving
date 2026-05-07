320b6e54fc024903366b0761a818f3d3f6c29033
Banibrata Santanu Manna <banibrata.manna@hotwaxsystems.com>
2026-05-07 16:16:11 +0530

    Refactored OFBiz api calls to Moqui Calls for return shipments
    Changelog: Changed

--- src/store/shipment.ts
+++ src/store/shipment.ts
@@ -180,6 +180,45 @@ export const useShipmentStore = defineStore("shipment", {
       return areAllSuccess;
     },
 
+    async receiveReturnShipment(payload: any) {
+      emitter.emit("presentLoader");
+      const productStore = useProductStore();
+      if (!payload.isMultiReceivingEnabled) {
+        payload.items = payload.items.filter((item: any) => item.quantityReceived === 0);
+      }
+      const items = payload.items.map((item: any) => {
+        return {
+          shipmentItemSeqId: item.itemSeqId,
+          productId: item.productId,
+          quantityAccepted: item.quantityAccepted,
+          orderId: item.orderId,
+          orderItemSeqId: item.orderItemSeqId,
+          unitCost: 0.0,
+          locationSeqId: item.locationSeqId,
+        };
+      });
+
+      try {
+        const resp = await api({
+          url: `oms/returnShipments/${payload.shipmentId}/receive`,
+          method: "POST",
+          data: {
+            shipmentId: payload.shipmentId,
+            items,
+            facilityId: productStore.getCurrentFacility.facilityId
+          }
+        });
+
+        if (resp && resp?.status === 200) {
+          return true;
+        }
+      } catch (err) {
+        commonUtil.showToast(translate("Something went wrong, please try again"));
+      }
+      emitter.emit("dismissLoader");
+      return false;
+    },
+
     async receiveShipmentJson(payload: any) {
       emitter.emit("presentLoader");
       const productStore = useProductStore();
